4784ef713ca04c15f964ed62b5d7528b930b93f7
Daniel McDougall <59933784+DanielMcDougall@users.noreply.github.com>
2020-10-02 04:32:27 -0600

    Add starting files for new heroku repo

---
+++ home.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="regs.css">
+    <link rel="small icon" href="favicon.ico">
+</head>
+
+<body>
+    <div id="wholepage">
+        <div id="topbar">
+            <a href="home.php"> <img id="logo" src="largewithname.png" alt="Song Share"> </a>
+          
+                <form class="search" action="results.php">
+                    <input type="text" placeholder="Search anything on Song Share" name="searchbar">
+                    <button type="submit"> Search</i></button>
+                </form>
+            <hr>
+        </div>
+        <div class="sidenav">
+            <a href="home.php">Home</a>
+            <a href="profile.php">Profile</a>
+            <!-- The following in the sidebar will have drop downs eventually, I think I need javascript?-->
+            <a href="songwriting.php">Songwriting</a>
+            <a href="music.php">Music</a>
+            <a href="multimedia.php">Multimedia</a>
+            <a href="albumartwork.php">Album Artwork</a>
+        </div>
+    </div>
+</body>
+
+</html>
